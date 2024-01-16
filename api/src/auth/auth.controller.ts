20b805b4904c67ab1f9ba0007a75492660716d7f
lseiller <lseiller@student.42.fr>
2024-01-16 18:31:45 +0100

    Add module auth structure

---
+++ api/src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUsersDto } from 'src/users/dto/create-users.dto';
+
+@ApiTags('authentification')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // @Post('register')
+  // createUser(@Body() body: CreateUsersDto) {
+  //   return this.authService.create(body);
+  // }
+
+//   @Get()
+//   findAll() {
+//     return this.authService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.authService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+//     return this.authService.update(+id, updateAuthDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.authService.remove(+id);
+//   }
+}
