5453940e622cddd780b96c0849e1f62b2129f4d0
lseiller <lseiller@student.42.fr>
2024-01-21 17:47:06 +0100

    Add friends ressources

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PongGateway } from './pong/pong.gateway';
 import { AuthModule } from './auth/auth.module';
+import { FriendsService } from './friends/friends.service';
+import { FriendsModule } from './friends/friends.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, FriendsModule],
   controllers: [AppController],
-  providers: [AppService, PongGateway],
+  providers: [AppService, PongGateway, FriendsService],
 })
 export class AppModule {}
