bf2c83891d30e27127ee226687c8a5c3451a7be0
“standlc” <s.delacomble@gmail.com>
2024-01-22 04:05:18 +0100

    delete test function

--- api/src/types/clientSchema.ts
+++ api/src/types/clientSchema.ts
@@ -2,7 +2,3 @@ import { User } from "./schema";
 import {Selectable} from "kysely"
 
 export type AppUser = Omit<Selectable<User>, 'password'>;
-
-export const hello = () => {
-    console.log("helo!")
-}
