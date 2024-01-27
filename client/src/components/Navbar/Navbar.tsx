03327e72c7c19f2e21fd7906e9ba5c35260f959f
Seth <vchan@student.42.fr>
2024-01-27 18:26:51 +0100

    animation-fixing button navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,39 +1,31 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [activeButton, setActiveButton] = useState(null);
 
-	return (
-		<div className="navbar bg-discord-black">
-			<button
-				className="mt-4 p-2 rounded-full bg-not-quite-black"
-				style={{ height: '60px', width: '60px' }}
-				>
-				user
-			</button>
-			{/*TODO: component for chan */}
-			<button
-				className="mt-4 p-2 rounded-full bg-blurple"
-				style={{ height: '60px', width: '60px' }}
-			>
-				chat 1
-			</button>
-			{/* TODO: Play component */}
-			<button
-				className="mt-4 p-2 rounded-full bg-blurple"
-				style={{ height: '60px', width: '60px' }}
-			>
-				Play
-			</button>
-			{/*TODO: invite component*/}
-			<button
-				className="mt-4 p-2 text-3xl text-green rounded-full bg-not-quite-black"
-				style={{ height: '60px', width: '60px' }}
-			>
-				+
-			</button>
+    const handleClick = (buttonText) => {
+        setActiveButton(buttonText);
+    };
 
-		</div>
-	);
+    const buttons = ["user", "chat 1", "Play", "+"];
+    return (
+        <div className="navbar bg-discord-black">
+            {buttons.map((buttonText, index) => (
+                <button
+                    key={index}
+                    className={`mt-4 p-2 rounded-full bg-not-quite-black ${
+                        activeButton === buttonText
+                            ? "rounded-lg bg-blurple hover:bg-blurple"
+                            : "hover:rounded-lg hover:bg-blurple"
+                    }`}
+                    style={{ height: "50px", width: "50px" }}
+                    onClick={() => handleClick(buttonText)}
+                >
+                    {buttonText}
+                </button>
+            ))}
+        </div>
+    );
 };
--- client/src/pages/PongGame.tsx
+++ client/src/pages/PongGame.tsx
@@ -1,76 +1,71 @@
 import { useEffect, useRef, useState } from "react";
 
 export default function PongGame() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [game, setGame] = useState({
-    ball: {
-      x: 200,
-      y: 100,
-    },
-    playerLeft: {
-      x: 0,
-      y: 0,
-    },
-    playerRight: {
-      x: 0,
-      y: 0,
-    },
-  });
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [game, setGame] = useState({
+        ball: {
+            x: 200,
+            y: 100,
+        },
+        playerLeft: {
+            x: 0,
+            y: 0,
+        },
+        playerRight: {
+            x: 0,
+            y: 0,
+        },
+    });
 
-  // const drawBall = (
-  //   ctx: CanvasRenderingContext2D,
-  //   x: number,
-  //   y: number,
-  //   w: number,
-  //   h: number
-  // ) => {
-  //   ctx.fillRect(x, y, h, w);
-  // };
+    // const drawBall = (
+    //   ctx: CanvasRenderingContext2D,
+    //   x: number,
+    //   y: number,
+    //   w: number,
+    //   h: number
+    // ) => {
+    //   ctx.fillRect(x, y, h, w);
+    // };
 
-  useEffect(() => {
-    if (!canvasRef.current) {
-      return;
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+
+        const ctx = canvasRef.current.getContext("2d");
+        if (!ctx) {
+            return;
+        }
+
+        ctx.fillStyle = "white";
+
+        ctx.fillRect(game.ball.x, game.ball.y, 25, 25);
+
+        ctx.fillRect(game.ball.x, game.ball.y, 25, 25);
+
+        // ctx?.fillRect(25, 25, 100, 100);
+        // ctx?.clearRect(45, 45, 60, 60);
+        // ctx?.strokeRect(50, 50, 50, 50);
+    }, [canvasRef, game]);
+
+    function roundedRect(
+        ctx: CanvasRenderingContext2D,
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        radius: number
+    ) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + radius);
+        ctx.arcTo(x, y + height, x + radius, y + height, radius);
+        ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+        ctx.arcTo(x + width, y, x + width - radius, y, radius);
+        ctx.arcTo(x, y, x, y + radius, radius);
+        ctx.stroke();
     }
 
-    const ctx = canvasRef.current.getContext("2d");
-    if (!ctx) {
-      return;
-    }
-
-    ctx.fillStyle = "white";
-
-    ctx.fillRect(game.ball.x, game.ball.y, 25, 25);
-
-    ctx.fillRect(game.ball.x, game.ball.y, 25, 25);
-
-    // ctx?.fillRect(25, 25, 100, 100);
-    // ctx?.clearRect(45, 45, 60, 60);
-    // ctx?.strokeRect(50, 50, 50, 50);
-  }, [canvasRef, game]);
-
-  function roundedRect(
-    ctx: CanvasRenderingContext2D,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    radius: number
-  ) {
-    ctx.beginPath();
-    ctx.moveTo(x, y + radius);
-    ctx.arcTo(x, y + height, x + radius, y + height, radius);
-    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
-    ctx.arcTo(x + width, y, x + width - radius, y, radius);
-    ctx.arcTo(x, y, x, y + radius, radius);
-    ctx.stroke();
-  }
-
-  return (
-    <canvas
-      ref={canvasRef}
-      width="600"
-      height="400"
-      className="border"
-    ></canvas>
-  );
+    return (
+        <canvas ref={canvasRef} width="600" height="400" className="border"></canvas>
+    );
 }
