a4d38f696f1a07e2c0cb5265948cd354451014c7
Seth <vchan@student.42.fr>
2024-02-03 15:16:31 +0100

    merging main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,83 +1,78 @@
-import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { DateTime } from "luxon";
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
 } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import PrivateLayout from "./components/PrivateLayout";
 import PublicLayout from "./components/PublicLayout";
 import PongGame from "./pages/PongGame";
-<<<<<<< HEAD
 import { Login } from "./pages/Login";
 import { Dashboard } from "./pages/Dashboard";
-=======
-import { AppUser } from "../../api/src/types/clientSchema";
->>>>>>> main
 
 const getUser = async () => {
-  const res = await axios.get<any>("/api");
-  return res.data;
+    const res = await axios.get<any>("/api");
+    return res.data;
 };
 
 function App() {
-  const [user, setUser] = useState<any | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState(true);
-  const [now, setNow] = useState("");
+    const [user, setUser] = useState<any | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState(true);
+    const [now, setNow] = useState("");
 
-  useEffect(() => {
-    const testApi = async () => {
-      const res: { data: string } = await axios.get("/api");
-      console.log(res.data);
-      setNow(DateTime.fromISO(res.data).toFormat("HH':' mm: ss"));
-      // const res = await axios.post<AppUser>("/api/auth/login", {
-      //   username: "john",
-      //   password: "123",
-      // });
-      // console.log(res.data);
-    };
-    testApi();
-  }, []);
+    useEffect(() => {
+        const testApi = async () => {
+            const res: { data: string } = await axios.get("/api");
+            console.log(res.data);
+            setNow(DateTime.fromISO(res.data).toFormat("HH':' mm: ss"));
+            // const res = await axios.post<AppUser>("/api/auth/login", {
+            //   username: "john",
+            //   password: "123",
+            // });
+            // console.log(res.data);
+        };
+        testApi();
+    }, []);
 
-  const { isPending, data } = useQuery({
-    queryKey: ["user"],
-    retry: false,
-    refetchOnWindowFocus: false,
-    queryFn: getUser,
-  });
+    const { isPending, data } = useQuery({
+        queryKey: ["user"],
+        retry: false,
+        refetchOnWindowFocus: false,
+        queryFn: getUser,
+    });
 
-  useEffect(() => {
-    if (data) {
-      setUser(data);
+    useEffect(() => {
+        if (data) {
+            setUser(data);
+        }
+        setIsLoading(isPending);
+    }, [data, isPending]);
+
+    if (isLoading) {
+        return "Loading...";
     }
-    setIsLoading(isPending);
-  }, [data, isPending]);
 
-  if (isLoading) {
-    return "Loading...";
-  }
-
-  return (
-    <RouterProvider
-      router={createBrowserRouter(
-        createRoutesFromElements(
-          <>
-<Route element={<PrivateLayout />}>
+    return (
+        <RouterProvider
+            router={createBrowserRouter(
+                createRoutesFromElements(
+                    <>
+                        <Route element={<PrivateLayout />}>
                             <Route index element={<Login />} />
                             {/* <Route path="/home" element={<Home now={now} user={user} />} /> */}
-                            <Route path="/home" element={<Dashboard now={now}/>} />
+                            <Route path="/home" element={<Dashboard now={now} />} />
                             <Route path="/play" element={<PongGame />} />
-                        </Route>  
-          </>
-        )
-      )}
-    />
-  );
+                        </Route>
+                    </>
+                )
+            )}
+        />
+    );
 }
-  
+
 export default App;
