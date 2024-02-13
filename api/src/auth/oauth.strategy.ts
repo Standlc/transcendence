ca7c08fd260a8adbc5ddbaaf2d3f1e2c434bf877
lseiller <lseiller@student.42.fr>
2024-02-13 17:38:04 +0100

    save

--- api/src/auth/oauth.strategy.ts
+++ api/src/auth/oauth.strategy.ts
@@ -2,21 +2,38 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-oauth2";
 import { AuthService } from "./auth.service";
+import { HttpService } from "@nestjs/axios";
+import { AxiosError, AxiosResponse } from "axios";
+import { catchError, firstValueFrom } from "rxjs";
 
 @Injectable()
 export class Oauth2Strategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private readonly httpService: HttpService) {
     super({
       authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
       tokenURL: 'https://api.intra.42.fr/oauth/token',
       clientID: 'u-s4t2ud-bfc3bd01981c3981040a3dad1e9dd27fb360884521e5e00d29a603717305c64a',
-      clientSecret: 'DO NOT COMMIT',
+      clientSecret: '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',
       callbackURL: 'http://localhost:5000/api/auth/redirect',
       scope: 'public'
     }); //config
   }
 
   async validate(accessToken: string) {
+    const config = {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    };
+
+    let test: AxiosResponse<string[]>;
+    test = await firstValueFrom(
+      this.httpService.get<string[]>('https://api.intra.42.fr/v2').pipe(
+        catchError((error: AxiosError) => {
+          console.log(error.response?.data);
+          throw "An error happened!";
+        }),
+      ),
+    );
+    console.log(test);
     console.log(accessToken);
     return 'test';
   }
