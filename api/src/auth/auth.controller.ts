30ab589ae0d05ef657e8214ffa37b9b264983388
lseiller <lseiller@student.42.fr>
2024-02-19 20:16:26 +0100

    Add swagger documentation

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, UseGuards, Request, Get, Res, UseFilters } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiDefaultResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiQuery, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { LocalAuthGuard } from './local-auth.guard';
 import { Response, Response as ResponseType } from 'express';
 import { AppUser } from 'src/types/clientSchema';
@@ -14,15 +14,22 @@ import { HttpExceptionFilter } from './redirect-exception.filter';
 export class AuthController {
   constructor(private readonly authService: AuthService, private readonly usersService: UsersService) {}
 
+  //#region oauth
+
   /**
    * GET /api/auth/oauth
    * This is the route the user will visit to authenticate using the 42 API
    */
+  @ApiDefaultResponse({description: "Nothing to return, the user will be redirected to the 42 API"})
   @Get('oauth')
   @UseGuards(OauthGuard)
   login() {
     return ;
   }
+
+  //#endregion
+
+  //#region redirect
 
   /**
    * GET /api/auth/redirect
@@ -30,22 +37,64 @@ export class AuthController {
    * We could do everything using just the /api/auth/oauth route. But this is less
    * complex to understand.
    */
-    @Get('redirect')
-    @UseGuards(OauthGuard)
-    @UseFilters(HttpExceptionFilter)
-    async redirect(@Request() req, @Res() res: Response) {
-      const token: string = await this.authService.login(req.user.id);
-      let date = new Date();
-      date.setDate(date.getDate() + 7);
-      res.cookie('token', token, {expires: date});
-      res.redirect('http://localhost:3000/');
-    }
+  @ApiQuery({
+    name: 'code',
+    required: true,
+    description: "User token of 42 API. Note: This endpoint should not be use directly, as you will never get a valid token for the 42 API. This endpoint purpose is solely for the 42 API."
+  })
+  @ApiOkResponse({
+    description: "This will redirect to either localhost:3000/ if a jwt as been generated, or localhost:3000/login?code=x if something went wrong. Note that x will be replaced with a string of the error."
+  }) 
+  @Get('redirect')
+  @UseGuards(OauthGuard)
+  @UseFilters(HttpExceptionFilter)
+  async redirect(@Request() req, @Res() res: Response) {
+    const token: string = await this.authService.login(req.user.id);
+    let date = new Date();
+    date.setDate(date.getDate() + 7);
+    res.cookie('token', token, {expires: date});
+    res.redirect('http://localhost:3000/');
+  }
+
+    //#endregion
+
+  //#region login
 
   /**
    * GET /api/auth/login
    * This is the route the user will visit to authenticate using an username
    * with a passord
    */
+  @ApiBody({
+    description: "User credential",
+    required: true,
+    schema: {
+      type: 'application/json',
+      example: {
+        username: "john",
+        password: "STRONGPASSWORD"
+      }
+    }
+  })
+  @ApiOkResponse({
+    description: "User logged in, a cookie will be added with a jwt token",
+    schema: {
+      type: 'object',
+      example: {
+        avatarUrl: null,
+        bio: null,
+        createdAt: "2024-02-16T14:28:58.410Z",
+        email: null,
+        firstname: "john",
+        id: 1,
+        lastname: "doe",
+        rating: 18,
+        username: "joe"
+      }
+    }
+  }) 
+  @ApiUnauthorizedResponse({description: "User credential is invalid"})
+  @ApiInternalServerErrorResponse({ description: "Whenever the backend fail in some point, probably an error with the db." })
   @UseGuards(LocalAuthGuard)
   @Get('login')
   async loginWithPassword(@Request() req, @Res({ passthrough: true }) res: ResponseType): Promise<AppUser> {
@@ -56,23 +105,53 @@ export class AuthController {
     return req.user;
   }
 
+  //#endregion
+
+  //#region token
+
   /**
    * GET /api/auth/token
    * This is the route the user will visit to authenticate using an existing
    * token
    */
+  @ApiOkResponse({
+    description: "User token is valid",
+    schema: {
+      type: 'object',
+      example: {
+        avatarUrl: null,
+        bio: null,
+        createdAt: "2024-02-16T14:28:58.410Z",
+        email: null,
+        firstname: "john",
+        id: 1,
+        lastname: "doe",
+        rating: 18,
+        username: "joe"
+      }
+    }
+  })
+  @ApiUnauthorizedResponse({description: "User token is invalid"})
+  @ApiInternalServerErrorResponse({ description: "Whenever the backend fail in some point, probably an error with the db." })
   @UseGuards(JwtAuthGuard)
   @Get('token')
   async loginWithToken(@Request() req): Promise<AppUser | undefined> {
     return await this.usersService.getUserById(req.user.id);
   }
 
+  //#endregion
+
+  //#region logout
+
   /**
    * GET /api/auth/logout
    * This is the route the user will visit to logout
    */
+  @ApiOkResponse({description: "User logged out"})
   @Get('logout')
   async logout(@Res({ passthrough: true }) res: ResponseType) {
     res.cookie('token', '', {expires: new Date() });
   }
+
+  //#endregion
 }
