b2737edf6badc4d2bf5a58da5357ed5a7e241367
lseiller <lseiller@student.42.fr>
2024-02-23 21:15:23 +0100

    Add status to AppUser

--- api/src/gameRequests/GameRequests.module.ts
+++ api/src/gameRequests/GameRequests.module.ts
@@ -5,9 +5,10 @@ import { PongGameModule } from 'src/pong/Pong.module';
 import { GamesModule } from 'src/games/Games.module';
 import { UsersService } from 'src/users/users.service';
 import { WsAuthGuard } from 'src/auth/ws-auth.guard';
+import { UsersStatusModule } from 'src/usersStatusGateway/UsersStatus.module';
 
 @Module({
-  imports: [GamesModule, PongGameModule],
+  imports: [GamesModule, PongGameModule, UsersStatusModule],
   controllers: [GameRequestsController],
   providers: [GameRequestsService, UsersService, WsAuthGuard],
   exports: [GameRequestsService],
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -2,15 +2,18 @@ import { Injectable, InternalServerErrorException, NotFoundException, Unauthoriz
 import { db } from 'src/database';
 import { CreateUsersDto } from './dto/create-users.dto';
 import * as bcrypt from 'bcrypt';
-import { AppUser, ListUsers } from 'src/types/clientSchema';
+import { AppUser, AppUserDB, ListUsers } from 'src/types/clientSchema';
 import { userFromIntra } from 'src/auth/oauth.strategy';
 import { randomBytes } from 'crypto';
 import { User } from 'src/types/schema';
 import { Selectable } from 'kysely';
 import { UpdateUsersDto } from './dto/update-users.dto';
+import { UsersStatusGateway } from 'src/usersStatusGateway/UsersStatus.gateway';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersStatusGateway: UsersStatusGateway) {}
+
   /**
    * Create a user in the database using createUserDto values
    * @param createUsersDto 
@@ -51,9 +54,9 @@ export class UsersService {
       console.log(error);
       throw new InternalServerErrorException();
     }
-    let user: AppUser | undefined;
+    let userDB: AppUserDB;
     try {
-      user = await db
+      userDB = await db
       .selectFrom('user')
       .select(['avatarUrl', 'bio', 'createdAt', 'email', 'firstname', 'id', 'lastname', 'rating', 'username'])
       .where('username', '=', createUsersDto.username)
@@ -62,7 +65,10 @@ export class UsersService {
       console.log(error);
       throw new InternalServerErrorException();
     }
-    return user;
+    return {
+      ...userDB,
+      status: this.usersStatusGateway.getUserStatus(userDB?.id)
+    };
   }
 
   /**
@@ -130,8 +136,11 @@ export class UsersService {
     if (!user)
       throw new NotFoundException();
     //? Create a AppUser containing every field, except password.
-    const {password, ...appUser} = user;
-    return appUser;
+    const {password, ...appUserDB} = user;
+    return {
+      ...appUserDB,
+      status: this.usersStatusGateway.getUserStatus(appUserDB?.id)
+    };
   }
 
   /**
@@ -144,7 +153,7 @@ export class UsersService {
   async findUsersByName(substring: string): Promise<AppUser[]> {
     //? Fetch the database and search for any user containing a substring in the username field.
     //? Create an array of AppUser containing every field except password of any user that matches the substring.
-    let user: AppUser[]
+    let user: AppUserDB[]
     try {
       user = await db
       .selectFrom('user')
@@ -158,7 +167,14 @@ export class UsersService {
     }
     if (!user)
       throw new NotFoundException();
-    return user;
+    let appUsers: AppUser[] = [];
+    user.forEach(user => {
+      appUsers.push({
+        ...user,
+        status: this.usersStatusGateway.getUserStatus(user?.id)
+      })
+    })
+    return appUsers;
   }
 
   /**
@@ -183,8 +199,11 @@ export class UsersService {
     }
     if (!user)
       throw new NotFoundException();
-    const {password, ...appUser} = user;
-    return appUser;
+    const {password, ...appUserDB} = user;
+    return {
+      ...appUserDB,
+      status: this.usersStatusGateway.getUserStatus(appUserDB?.id)
+    };
   }
 
   /**
@@ -195,7 +214,7 @@ export class UsersService {
    * @throws InternalServerError if we fail to use the db.
    */
   async getUserByEmail(email: string): Promise<AppUser> {
-    let user: AppUser | undefined;
+    let user: AppUserDB | undefined;
     try {
       user = await db
       .selectFrom('user')
@@ -208,7 +227,10 @@ export class UsersService {
     }
     if (!user)
       throw new NotFoundException();
-    return user;
+    return {
+      ...user,
+      status: this.usersStatusGateway.getUserStatus(user?.id)
+    };
   }
 
   /**
@@ -234,8 +256,11 @@ export class UsersService {
       if (!result)
         throw new UnauthorizedException();
 
-      const {password, ...appUser} = user;
-      return appUser;
+      const {password, ...appUserDB} = user;
+      return {
+        ...appUserDB,
+        status: this.usersStatusGateway.getUserStatus(appUserDB?.id)
+      };
     } catch (error) {
       if (error instanceof UnauthorizedException || error instanceof NotFoundException)
         throw new UnauthorizedException;
