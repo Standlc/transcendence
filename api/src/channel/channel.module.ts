6ccc6dca2ab1fdff4afd5090e821434b02bd22e8
lseiller <lseiller@student.42.fr>
2024-02-28 12:03:54 +0100

    Import module for channel and dm

--- api/src/channel/channel.module.ts
+++ api/src/channel/channel.module.ts
@@ -5,8 +5,11 @@ import { UserController } from './channel.controller';
 import { WsAuthGuard } from 'src/auth/ws-auth.guard';
 import { ConnectedUsersService } from 'src/connectedUsers/connectedUsers.service';
 import { FriendsService } from 'src/friends/friends.service';
+import { FriendsModule } from 'src/friends/friends.module';
+import { UsersStatusModule } from 'src/usersStatusGateway/UsersStatus.module';
 
 @Module({
+  imports: [FriendsModule, UsersStatusModule],
   controllers: [UserController],
   providers: [
     ChannelGateway,
