21abd269b6dc30d6cfd7ce07f80cbbe44e7da493
Seth <visethgchan@gmail.com>
2024-02-29 05:33:02 +0100

    pour aki

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,15 +6,18 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('Transcendence API')
-  .setDescription('API documentation for transcendence')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Transcendence API')
+    .setDescription('API documentation for transcendence')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('/docs', app, document);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000', // Your frontend origin
+    credentials: true, // To allow sending credentials like cookies, authorization headers with requests
+  });
   app.setGlobalPrefix('api');
   await app.listen(5000);
 }
