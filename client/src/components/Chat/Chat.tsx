829868cda60c25d75b43aee57724ff797fd435e0
Seth <visethgchan@gmail.com>
2024-02-29 15:45:01 +0100

    good version

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -366,7 +366,7 @@ const Chat: React.FC<Props> = ({
                     onChange={(e) => setMessage(e.target.value)}
                     placeholder="Write a message..."
                 />
-                <button type="submit">Send</button>
+                <button type="submit">Sendddd</button>
             </form>
         </div>
     );
