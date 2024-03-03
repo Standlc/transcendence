b998eb59b9285abe6d4db5903416310400e8326e
lseiller <lseiller@student.42.fr>
2024-03-03 15:20:56 +0100

    Add 2FA status in the jwt payload, Add function to generate 2FA secret Add function to verify the 2FA secret from the user Add function to create a Qrcode

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import { AppUser } from 'src/types/clientSchema';
+import { AppUser, AppUserDB } from 'src/types/clientSchema';
 import { userFromIntra } from './oauth.strategy';
+import { authenticator } from 'otplib';
+import { toDataURL } from 'qrcode';
+import { User } from 'src/types/schema';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +13,8 @@ export class AuthService {
 
   /**
    * Validate if the user we receive exist and if the password match.
-   * @param username 
-   * @param password 
+   * @param username
+   * @param password
    * @returns AppUser
    */
   async validateUser(username: string, password: string): Promise<AppUser> {
@@ -20,7 +23,7 @@ export class AuthService {
 
   /**
    * Validate if the email we receive exist in our db.
-   * @param email 
+   * @param email
    * @returns AppUser -> pass throw if no AppUser was found.
    */
   async validateEmail(email: string): Promise<AppUser> {
@@ -29,7 +32,7 @@ export class AuthService {
 
   /**
    * Wrap userService createOauthUser
-   * @param intraUser 
+   * @param intraUser
    * @returns true or false
    */
   async registerOauth(intraUser: userFromIntra) {
@@ -41,8 +44,75 @@ export class AuthService {
    * @param id
    * @returns signed jwt
    */
-  async login(id: number): Promise<string> {
-    const payload = {id: id};
+  async login(userId: number): Promise<string> {
+    const payload = {
+      id: userId,
+      isTwoFactorAuthenticated: false,
+    };
+
+    return await this.jwtService.signAsync(payload);
+  }
+
+  /**
+   * Generate a secret that will be used to validate a set of 6 number.
+   * Generate an otpAuthUrl that contain the secret, the username and name of
+   * our application, this will be send to the front end later in a form of
+   * qrcode.
+   * Storing the secret in the user table.
+   * @param userId
+   * @returns otpAuthUrl
+   * @throws InternalServerError
+   */
+  async generateTwoFactorAuthenticationSecret(userId: number): Promise<string> {
+    try {
+      const user = await this.usersService.getUserById(userId);
+      if (user.isTwoFactorAuthenticationEnabled)
+        throw new UnprocessableEntityException("This user already activate 2FA.");
+      const secret = authenticator.generateSecret();
+      const otpAuthUrl = authenticator.keyuri(user.username, "Transcendence", secret);
+
+      await this.usersService.setTwoFactorAuthenticationSecret(user.id, secret);
+
+      return otpAuthUrl;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  /**
+   * Generate a qrcode using qrcode lib.
+   * @param otpAuthUrl
+   * @returns PNG Image formated as base64 string
+   */
+  async generateQrCodeDataUrl(otpAuthUrl: string): Promise<string> {
+    return await toDataURL(otpAuthUrl);
+  }
+
+  /**
+   * Verify the code the 2FA app user generate with the code that will be
+   * generate from the backend.
+   * @param TwoFactorAuthenticationCode
+   * @param userId
+   * @returns true or false, depending if the code was valid
+   */
+  async isTwoFactorAuthenticationCodeValid(TwoFactorAuthenticationCode: string, userId: number) {
+    return authenticator.verify({
+      token: TwoFactorAuthenticationCode,
+      secret: await this.usersService.getTwoFactorAuthenticationSecret(userId)
+    })
+  }
+
+  /**
+   * Create a JWT using a payload and signing it.
+   * @param userId
+   * @returns
+   */
+  async loginWith2fa(userId: number): Promise<string> {
+    const payload = {
+      id: userId,
+      isTwoFactorAuthenticated: true,
+    }
 
     return await this.jwtService.signAsync(payload);
   }
