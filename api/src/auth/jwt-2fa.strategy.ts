5e69fa4f3185be614581312127015ebea2862286
lseiller <lseiller@student.42.fr>
2024-03-03 15:48:20 +0100

    Add guard for jwt-2fa Update jwt guard to handle the case when a user did not validate their 2fa code

--- api/src/auth/jwt-2fa.strategy.ts
+++ api/src/auth/jwt-2fa.strategy.ts
@@ -6,11 +6,11 @@ import { AppUser } from "src/types/clientSchema";
 import { UsersService } from "src/users/users.service";
 
 @Injectable()
-export class jwtStrategy extends PassportStrategy(Strategy) {
+export class Jwt2faStrategy extends PassportStrategy(Strategy, 'jwt-2fa') {
   constructor(private usersService: UsersService) {
     super({
       secretOrKey: process.env.JWT_SECRET,
-      jwtFromRequest: jwtStrategy.extractJwtFromCookie,
+      jwtFromRequest: Jwt2faStrategy.extractJwtFromCookie,
       ignoreExpiration: false
     })
   }
@@ -19,28 +19,16 @@ export class jwtStrategy extends PassportStrategy(Strategy) {
    * This is our custom jwt extractor from cookie
    * @param req
    * @returns token or null
-   */
+  */
   private static extractJwtFromCookie(req: Request): string | undefined {
     if (req && req.cookies && 'token' in req.cookies && req.cookies.token.length > 0) {
       return req.cookies.token;
     }
   }
 
-  /**
-   * This function acutally doesnt validate anything as jwt handle the
-   * validation of the extracted token. We just validate if the id exist in our
-   * database. Someone could have a valid token that was issue for an account
-   * that has been deleted. Token is valid for seven days.
-   * Someone who is having a valid token without a user id field should never
-   * occurs, it probably means someone succesfully generate a token, they might
-   * know the JWT_SECRET we use
-   * @param payload
-   * @returns UserID
-   * @throws UnauthorizedException
-   */
   async validate(payload: {
     id: number,
-    isTwoFactorAuthenticated: boolean
+    isTwoFactorAuthenticated: boolean,
   }): Promise<{ id: number } | undefined> {
     try {
       if (!payload.id) {
@@ -48,8 +36,6 @@ export class jwtStrategy extends PassportStrategy(Strategy) {
         throw "Missing information from the payload";
       }
       const user = await this.usersService.getUserById(payload.id);
-      if (user.isTwoFactorAuthenticationEnabled && !payload.isTwoFactorAuthenticated)
-        throw "Missing 2FA from the payload";
       return { id: user.id };
     } catch (error) {
       console.log(error);
