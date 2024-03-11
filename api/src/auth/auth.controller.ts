ddf3fcdc8ff67f8c54aa1d51679f6db03ddcfb4a
lseiller <lseiller@student.42.fr>
2024-03-11 18:04:43 +0100

    Update Partial AppUser to AppUser

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -112,7 +112,7 @@ export class AuthController {
   @ApiInternalServerErrorResponse({ description: "Whenever the backend fail in some point, probably an error with the db." })
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async loginWithPassword(@Request() req, @Res({ passthrough: true }) res: ResponseType): Promise<Partial<AppUser>> {
+  async loginWithPassword(@Request() req, @Res({ passthrough: true }) res: ResponseType): Promise<AppUser> {
     if (!req.user.id)
       throw new UnauthorizedException("Missing Id from the payload");
     const session: {
@@ -124,8 +124,10 @@ export class AuthController {
       sameSite: 'strict',
       httpOnly: true
     });
-    if (req.user.isTwoFactorAuthenticationEnabled)
-      return { isTwoFactorAuthenticationEnabled: req.user.isTwoFactorAuthenticationEnabled };
+    if (req.user.isTwoFactorAuthenticationEnabled) {
+      let emptyUser = <AppUser>{ isTwoFactorAuthenticationEnabled: req.user.isTwoFactorAuthenticationEnabled };
+      return emptyUser;
+    }
     return req.user;
   }
 
